7bf6069ad97aae66d398d6a351ed386ae9cc63bf
Ian Schoonover <iantskon@gmail.com>
2019-07-13 16:35:10 -0500

    Update script to only run on course pages

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 var affiliateStuff = '?ranMID=39197&ranEAID=O*yDCFDQTmY&ranSiteID=O.yDCFDQTmY-pbBV5G_YA1uxwpEcfTPfUg&LSNPUBID=O*yDCFDQTmY';
 var link = window.location.href;
-if (link.includes(affiliateStuff)) {
-  console.log('Already included');
-} else {
-  console.log('Not included');
+if (!link.includes(affiliateStuff) && link.split('/').length === 5 && !link.includes('https://www.udemy.com/message/')) {
   window.location.href = link+=affiliateStuff;
 }
