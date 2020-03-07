1729b1efa0c303fe7b2c3f89993e99c740765c3d
Ian Schoonover <iantskon@gmail.com>
2020-03-07 15:29:23 -0600

    Add support for links without question id

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # DevSprout Udemy Affiliate Chrome Extension
 
+**Disclaimer**
+By using this extension you acknowledge that any course you purchase from Udemy.com will result in me (Ian Schoonover) making a commission due to the affiliate query string added to the url. 
+Any course you purchase will have a 30-day money back guarantee from Udemy.
+
 ## Instructions
 1. Download (and unzip) or clone this repo
 2. Open chrome browser and type "chrome:extensions" in the address bar
@@ -9,7 +13,5 @@
 6. The extension is now loaded in chrome and will execute automatically
 7. Thank you! :)
 
-### Disclaimer
-
-By using this extension you acknowledge that any course you purchase from Udemy.com will result in me (Ian Schoonover) making a commission due to the affiliate query string added to the url. 
-Any course you purchase will have a 30-day money back guarantee from Udemy.
+### Updates
+- 03/07/2020 The extension now fixes most cases of the Udemy Q&A link bug, see [here](https://www.loom.com/share/5eb8d0658a444ffba07ca081d04f1f05) to learn more about the bug. Please note, some url patterns will only work for The Ultimate MySQL Bootcamp and The Web Developer Bootcamp (e.g., a link without a question id in it).
--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 var affiliateStuff = '?ranMID=39197&ranEAID=O*yDCFDQTmY&ranSiteID=O.yDCFDQTmY-pbBV5G_YA1uxwpEcfTPfUg&LSNPUBID=O*yDCFDQTmY';
-var re = new RegExp(/https\:\/\/www\.udemy\.com\/course\/([\w-]+)\/*/);
+var link = window.location.href;
+var re0 = new RegExp(/https\:\/\/www\.udemy\.com\/course\/([\w-]+)\/*/);
+var re1 = new RegExp(/https\:\/\/www\.udemy\.com\/course\/([\w-]+)\/learn\/#questions\/\d*\/*/);
+if(re1.test(link)) {
+  var kebab = new RegExp(/(\w+)-(\w)([\w-]*)/);
+  var courseSlug = link.match(kebab)[0]
+  var questionId = courseSlug === 'the-web-developer-bootcamp' ? '3861710' : '6912202';
+  link = link.replace(/https\:\/\/www\.udemy\.com\/course\/([\w-]+)\/learn\//, 'https://www.udemy.com/course/'+courseSlug+'/learn/lecture/'+questionId);
+}
 var re2 = new RegExp(/https\:\/\/www\.udemy\.com\/course\/([\w-]+)\/learn\/lecture\/\d+#/);
 var re3 = new RegExp(/https\:\/\/www\.udemy\.com\/course\/([\w-]+)\/learn\/lecture\/\d+#overview/)
-var link = window.location.href;
 if (re2.test(link) && !re3.test(link)) {
   setTimeout(function() {
     window.location.href = link;
   }, 1000)
-} else if(!link.includes(affiliateStuff) && re.test(link) && !re3.test(link) && !link.includes('https://www.udemy.com/message/')) {
+} else if(!link.includes(affiliateStuff) && re0.test(link) && !re3.test(link) && !link.includes('https://www.udemy.com/message/')) {
   window.location.href = link+=affiliateStuff;
 }
