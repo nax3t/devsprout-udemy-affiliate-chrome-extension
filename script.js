fde85d9caa27cb626af0c62857d2227c77c15d83
Ian Schoonover <iantskon@gmail.com>
2020-03-07 14:37:10 -0600

    Update extension to include Q&A link fix

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var affiliateStuff = '?ranMID=39197&ranEAID=O*yDCFDQTmY&ranSiteID=O.yDCFDQTmY-pbBV5G_YA1uxwpEcfTPfUg&LSNPUBID=O*yDCFDQTmY';
+var re = new RegExp(/https\:\/\/www\.udemy\.com\/course\/([\w-]+)\/*/);
+var re2 = new RegExp(/https\:\/\/www\.udemy\.com\/course\/([\w-]+)\/learn\/lecture\/\d+#/);
+var re3 = new RegExp(/https\:\/\/www\.udemy\.com\/course\/([\w-]+)\/learn\/lecture\/\d+#overview/)
 var link = window.location.href;
-if (!link.includes(affiliateStuff) && link.split('/').length === 5 && !link.includes('https://www.udemy.com/message/')) {
+if (re2.test(link) && !re3.test(link)) {
+  setTimeout(function() {
+    window.location.href = link;
+  }, 1000)
+} else if(!link.includes(affiliateStuff) && re.test(link) && !re3.test(link) && !link.includes('https://www.udemy.com/message/')) {
   window.location.href = link+=affiliateStuff;
 }
